74d5cba3cd198abed334ed17cba91e7fbd20bb57
FrankH <4848285@qq.com>
2016-12-01 23:45:44 +0800

    Init test proj and appveyor

---
+++ test/Myvas.AspNetCore.Tests/Collections/Extensions/ListExtensions_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Myvas.AspNetCore.Tests.Collections.Extensions
+{
+    public class ListExtensions_Tests
+    {
+        [Fact]
+        public void Should_SortByDependencies()
+        {
+        }
+    }
+}
---
+++ test/Myvas.AspNetCore.Tests/Collections/TypeList_Test.cs
@@ -0,0 +1,38 @@
+﻿using Myvas.AspNetCore.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Myvas.AspNetCore.Tests
+{
+    public class TypeList_Test
+    {
+        [Fact]
+        public void ShouldOnlyAddTrueTypes()
+        {
+            var list = new TypeList<IMyInterface>();
+        }
+
+        public interface IMyInterface
+        {
+
+        }
+
+        public class MyClass1 : IMyInterface
+        {
+
+        }
+
+        public class MyClass2 : IMyInterface
+        {
+
+        }
+
+        public class MyClass3
+        {
+
+        }
+    }
+}
